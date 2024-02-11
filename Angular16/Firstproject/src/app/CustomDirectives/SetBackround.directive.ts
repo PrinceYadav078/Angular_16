d8888cb35017dd692ee5a035d6438aa2827332d1
Prince Yadav <prince.yadav@brainwaveconsulting.co.in>
2024-02-12 04:54:55 +0530

    Property binding in Custom  Directives

--- Angular16/Firstproject/src/app/CustomDirectives/SetBackround.directive.ts
+++ Angular16/Firstproject/src/app/CustomDirectives/SetBackround.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, Input, Renderer2 } from "@angular/core";
 
 
 @Directive({
@@ -7,6 +7,11 @@ import { Directive, ElementRef, Renderer2 } from "@angular/core";
 export class SetBackground{
     // private element:ElementRef
     // private renderer:Renderer2
+
+    // @Input('setBackground') backcolor:string='#36454F'
+    // @Input() textcolor:string='white'
+
+    @Input('setBackground') changeBackandText ={backcolor:'#36454F', textcolor:'white'}
 
     constructor(private element:ElementRef, private renderer:Renderer2){
     //    this.element=element;
@@ -18,8 +23,8 @@ export class SetBackground{
         // this.element.nativeElement.style.color= 'white';
 
 
-        this.renderer.setStyle(this.element.nativeElement,'backgroundColor','#36454F')
-        this.renderer.setStyle(this.element.nativeElement,'color','white')
+        this.renderer.setStyle(this.element.nativeElement,'backgroundColor',this.changeBackandText.backcolor)
+        this.renderer.setStyle(this.element.nativeElement,'color',this.changeBackandText.textcolor)
         // this.renderer.setAttribute(this.element.nativeElement,'title','this is gbc element')
     }
 }
