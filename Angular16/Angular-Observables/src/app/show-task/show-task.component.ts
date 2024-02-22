1a6f01fb2e093ea1884caf058d4811770b7ea6c6
Prince Yadav <prince.yadav@brainwaveconsulting.co.in>
2024-02-23 03:32:55 +0530

    communication B/W non related components by using Subject and Services

---
+++ Angular16/Angular-Observables/src/app/show-task/show-task.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { TaskService } from '../Services/task.service';
+
+@Component({
+  selector: 'app-show-task',
+  templateUrl: './show-task.component.html',
+  styleUrls: ['./show-task.component.css']
+})
+export class ShowTaskComponent {
+  tasks: string[] = ['task 1', 'task 2', 'task 3']
+
+  taskService= inject(TaskService);
+  
+  ngOnInit(){
+    this.taskService.CreateTask.subscribe((newTask)=>{
+      this.tasks.push(newTask);
+   
+    })
+  }
+}
