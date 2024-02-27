7f3a6dc53c7f0fba5daf733ec750d340e81355b8
Prince Yadav <prince.yadav@brainwaveconsulting.co.in>
2024-02-27 06:18:25 +0530

    Replay Subject

--- Angular16/Angular-Observables/src/app/subjects/subjects.component.ts
+++ Angular16/Angular-Observables/src/app/subjects/subjects.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 @Component({
   selector: 'app-subjects',
@@ -11,27 +11,37 @@ export class SubjectsComponent {
 
   ngOnInit() {
     // let obs= new Observable((observer)=>{observer.next(Math.random())})
-    const sub = new Subject();
-    const Behv_subject = new BehaviorSubject<number>(100);
+    // const sub = new Subject();
+    // const Behv_subject = new BehaviorSubject<number>(100);
+    const replay_subject= new ReplaySubject();
+
+    replay_subject.next(100);
+    replay_subject.next(200);
+    replay_subject.next(300);
 
     //Subscriber 1
     // obs.subscribe((val)=>{console.log(val)})
     // sub.subscribe((val) => console.log(val));
-    Behv_subject.subscribe((val)=>{console.log("Subcribr1=> ",val)});
+    // Behv_subject.subscribe((val)=>{console.log("Subcribr1=> ",val)});
+    replay_subject.subscribe((val)=>{console.log("Subcribr1=> ", val)})
 
     //Subscriber 2
     // obs.subscribe((val)=>{console.log(val)})
     // sub.subscribe((val) => console.log(val));
-    Behv_subject.subscribe((val)=>{console.log("Subcribr2=> ",val)});
+    // Behv_subject.subscribe((val)=>{console.log("Subcribr2=> ",val)});
+    replay_subject.subscribe((val)=>{console.log("Subcribr2=> ", val)})
 
-    Behv_subject.next(2018);
+    // Behv_subject.next(2018);
+    replay_subject.next(2018)
 
     //Subscriber 3
-    Behv_subject.subscribe((val)=>{console.log("Subcribr3=> ",val)});
+    // Behv_subject.subscribe((val)=>{console.log("Subcribr3=> ",val)});
+    replay_subject.subscribe((val)=>{console.log("Subcribr3=> ", val)})
 
-    Behv_subject.next(2023)
+    // Behv_subject.next(2023)
+    replay_subject.next(2023)
 
-    sub.next(Math.random());
+    // sub.next(Math.random());
 
     const data = ajax('https://randomuser.me/api/');
     const subject= new Subject();
