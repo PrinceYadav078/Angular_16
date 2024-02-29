4464123ecf0e440cf6223e3639465e6ebc019642
Prince Yadav <prince.yadav@brainwaveconsulting.co.in>
2024-03-01 02:56:56 +0530

    implement a scrolling effect based on the user preference by using fragment and scrollIntoView method

--- Angular16/Angular-Routing/src/app/home/home.component.ts
+++ Angular16/Angular-Routing/src/app/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  activeRoute:ActivatedRoute=inject(ActivatedRoute);
+
+  ngOnInit(){
+    this.activeRoute.fragment.subscribe((data)=>{
+      // console.log(data)
+      if(data != null)
+      this.jumpToSection(data);
+    })
+  }
+
+  jumpToSection(section_id){
+    document.getElementById(section_id).scrollIntoView({behavior:'smooth'})
+  }
 }
