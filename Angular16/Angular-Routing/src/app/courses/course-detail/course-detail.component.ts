628a10558c733bcf528dd1a8632ad64533c9cabc
Prince Yadav <prince.yadav@brainwaveconsulting.co.in>
2024-02-29 06:27:19 +0530

    Implement functionality of nextCourse and previous course by using paramMap Observable

--- Angular16/Angular-Routing/src/app/courses/course-detail/course-detail.component.ts
+++ Angular16/Angular-Routing/src/app/courses/course-detail/course-detail.component.ts
@@ -1,29 +1,45 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Course } from 'src/app/Model/course';
 import { CourseService } from 'src/app/Services/course.service';
 
 @Component({
   selector: 'app-course-detail',
   templateUrl: './course-detail.component.html',
-  styleUrls: ['./course-detail.component.css']
+  styleUrls: ['./course-detail.component.css'],
 })
-export class CourseDetailComponent implements OnInit{
+export class CourseDetailComponent implements OnInit, OnDestroy {
   selectedCourse: Course;
   courseId: number;
 
   courseService: CourseService = inject(CourseService);
   activeRoute: ActivatedRoute = inject(ActivatedRoute);
+  paramMapObs;
 
-  ngOnInit(){
+  ngOnInit() {
     // this.courseId= this.activeRoute.snapshot.params['id']
-    this.courseId=+this.activeRoute.snapshot.paramMap.get('id')  // + sign convert the stringtype value to number type
+    // this.courseId=+this.activeRoute.snapshot.paramMap.get('id')  // + sign convert the stringtype value to number type
     // console.log(this.courseId)
-    this.selectedCourse=this.courseService.courses.find((course)=>{
-      return course.id === this.courseId
-    })
-    // console.log(this.selectedCourse)
-      
-    
+
+    // this.activeRoute.params.subscribe((data) => {
+    //   this.courseId = data['id'];
+    //   this.selectedCourse = this.courseService.courses.find((course) => {
+    //     return course.id == this.courseId;
+    //   });
+    //   // console.log(this.selectedCourse)
+    // });
+
+    this.paramMapObs = this.activeRoute.paramMap.subscribe((data) => {
+      this.courseId = +data.get('id'); // + sign convert the stringtype value to number type
+      this.selectedCourse = this.courseService.courses.find((course) => {
+        return course.id === this.courseId;
+      });
+      // console.log(this.selectedCourse)
+    });
+  }
+
+  ngOnDestroy() {
+    this.paramMapObs.unsubscribe();
   }
 }
