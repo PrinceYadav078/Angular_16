ac3f25f3d8661157cab7ea61ffebaa34522a4660
Prince Yadav <prince.yadav@brainwaveconsulting.co.in>
2024-03-04 05:23:28 +0530

    implement loader functinallity by using Navigation Events

--- Angular16/Angular-Routing/src/app/app.component.ts
+++ Angular16/Angular-Routing/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  Event,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-Routing';
+
+  showLoader: boolean = false;
+  router: Router = inject(Router);
+  ngOnInit() {
+    this.router.events.subscribe((routerEvent: Event) => {
+      if (routerEvent instanceof NavigationStart) {
+        this.showLoader = true;
+      }
+      if (
+        routerEvent instanceof NavigationEnd ||
+        routerEvent instanceof NavigationCancel ||
+        routerEvent instanceof NavigationError
+      ) {
+        this.showLoader = false;
+      }
+    });
+  }
 }
