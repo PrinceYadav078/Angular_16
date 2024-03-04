5c953b48ec2f52d0e9c7bca666a3621820e64d2a
Prince Yadav <prince.yadav@brainwaveconsulting.co.in>
2024-03-04 05:53:51 +0530

    Passing data with a route

--- Angular16/Angular-Routing/src/app/checkout/checkout.component.ts
+++ Angular16/Angular-Routing/src/app/checkout/checkout.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Course } from '../Model/course';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit{
+
+course;
+
+router:Router=inject(Router)
+
+ngOnInit(){
+  this.course= history.state
+  console.log(this.course)
+}
 
 }
