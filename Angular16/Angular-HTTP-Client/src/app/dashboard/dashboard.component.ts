d6bcbc0694307bb4cd0d7d5103c66d2de29593b5
Prince Yadav <prince.yadav@brainwaveconsulting.co.in>
2024-03-07 00:24:14 +0530

    implement post and get request by HttpClient and store task in database and fetch all task from it

--- Angular16/Angular-HTTP-Client/src/app/dashboard/dashboard.component.ts
+++ Angular16/Angular-HTTP-Client/src/app/dashboard/dashboard.component.ts
@@ -1,23 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Task } from '../Model/Task';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
   showCreateTaskForm: boolean = false;
 
-  OpenCreateTaskForm(){
+  http: HttpClient = inject(HttpClient);
+
+  ngOnInit(){
+    this.fetchAllTasks()
+  }
+
+  OpenCreateTaskForm() {
     this.showCreateTaskForm = true;
   }
 
-  CloseCreateTaskForm(){
+  CloseCreateTaskForm() {
     this.showCreateTaskForm = false;
   }
 
-  CreateTask(data:Task){
+  CreateTask(data: Task) {
+    const header = new HttpHeaders({ 'my-header': 'create-task' });
     console.log(data);
+    this.http.post<{name:string}>(
+      'https://angularhttpclient-11bb8-default-rtdb.firebaseio.com/tasks.json',
+      data,
+      { headers: header }
+    ).subscribe((response)=>{
+      console.log(response)
+    });
+  }
+
+  fetchAllTasks(){
+    this.http.get<{[key:string]:Task}>('https://angularhttpclient-11bb8-default-rtdb.firebaseio.com/tasks.json').pipe(map((response)=>{
+      //TRANSFORM DATA
+      let tasks=[];
+      for(let key in response){
+        if(response.hasOwnProperty(key)){
+          tasks.push({...response[key], id:key})
+        }
+      }
+      return tasks;
+    })).subscribe((tasks)=>{
+      console.log(tasks);
+    })
   }
 }
